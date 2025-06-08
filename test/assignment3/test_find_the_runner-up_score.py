8c9dc11e304e4ddcd2da1254c0e6a2820e8fa0e4
diakar-bytewise <divakar.c@diggibyte.com>
2025-06-08 18:46:06 +0530

    added unit tests for finding second largest function

---
+++ test/assignment3/test_find_the_runner-up_score.py
@@ -0,0 +1,19 @@
+import unittest
+from src.assignment1.util import finding_second_largest
+
+class TestFindingSecondLargest(unittest.TestCase):
+
+    def test_case_1(self):
+        self.assertEqual(finding_second_largest([2,3,6,6,5]),5)
+
+    def test_case_2(self):
+        self.assertEqual(finding_second_largest([1,2,3,4,5]),4)
+
+    def test_case_3(self):
+        self.assertEqual(finding_second_largest([10,10,9]),9)
+
+    def test_case_4(self):
+        self.assertEqual(finding_second_largest([5,5,5,5,4]),4)
+
+if __name__ == '__main__':
+    unittest.main()
