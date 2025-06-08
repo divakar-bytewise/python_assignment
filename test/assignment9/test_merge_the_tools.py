4003bc1fe8ac5d4a3895d3e4f7e9f76b2daee291
diakar-bytewise <divakar.c@diggibyte.com>
2025-06-08 19:44:02 +0530

    added unit test for merge the tools function

---
+++ test/assignment9/test_merge_the_tools.py
@@ -0,0 +1,17 @@
+import unittest
+import io
+import sys
+from src.assignment1.util import merge_the_tools
+
+class TestMergeTheTools(unittest.TestCase):
+    def test_merge_the_tools(self):
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        merge_the_tools("AABCAAADA", 3)
+        sys.stdout = sys.__stdout__
+        result = captured_output.getvalue().strip().split('\n')
+        expected = ['AB', 'CA', 'AD']
+        self.assertEqual(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
