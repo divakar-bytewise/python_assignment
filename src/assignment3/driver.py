2a1ec7940fe920fe5520949754914cf9155ca2a1
diakar-bytewise <divakar.c@diggibyte.com>
2025-06-08 18:58:23 +0530

    ading function to find second largest number from list with driver script

--- src/assignment3/driver.py
+++ src/assignment3/driver.py
@@ -1,4 +1,4 @@
-from src.assignment1.util import finding_second_largest
+from python_assignment.src.assignment3.util import finding_second_largest
 
 if __name__ == '__main__':
     n=int(input("Enter number of elements: "))
