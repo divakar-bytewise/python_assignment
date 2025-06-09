2c914dd122f185fa6cdb5b5e8e73dcdb81629f1e
diakar-bytewise <divakar.c@diggibyte.com>
2025-06-09 11:28:37 +0530

    implemented cube stacking logic with driver scripts

--- src/assignment13/util.py
+++ src/assignment13/util.py
@@ -17,8 +17,10 @@ def can_stack_cubes(cubes):
     return "Yes"
 
 
-for _ in range(t):
-    t=int(input())
-    n = int(input())
-    cubes = list(map(int, input().split()))
-    print(can_stack_cubes(cubes))
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        cubes = list(map(int, input().split()))
+        print(can_stack_cubes(cubes))
